a88e9b1c488accbb26ec806c6ef01a4d70de9c5b
Anna <0301annaumnova@gmail.com>
2022-03-21 23:02:49 +0300

    version 2

--- src/test/kotlin/lab1/Lab1MainKtTest.kt
+++ src/test/kotlin/lab1/Lab1MainKtTest.kt
@@ -5,24 +5,41 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
 
 internal class Lab1MainKtTest {
-
+    //    private val bookOne = BooksLib("Мы","Замятин Е.И.","1924")
+    //    private val bookTwo = BooksLib("Ионыч","Чехов А.П.","1898")
     @Test
     fun parseBookList() {
+        val firstBook = "1.Мы//Замятин Е.И.//1924"
+        assertEquals(
+            listOf(BooksLib("Мы", "Замятин Е.И.", "1924")),
+            parseBookList(firstBook)
+        )
+
+        val secondBook = "1.Мы//Замятин Е.И.//1924\n" +
+                "2.Ионыч//Чехов А.П.//1898"
+        assertEquals(
+            listOf(
+                BooksLib("Мы", "Замятин Е.И.", "1924"),
+                BooksLib("Ионыч", "Чехов А.П.", "1898")
+            ),
+            parseBookList(secondBook)
+        )
+
     }
 
-    @Test
-    fun searchNewBook() {
-    }
-
-    @Test
-    fun searchOldBook() {
-    }
-
-    @Test
-    fun searchLongNamed() {
-    }
-
-    @Test
-    fun searchShortNamed() {
-    }
+//    @Test
+//    fun searchNewBook() {
+//    }
+//
+//    @Test
+//    fun searchOldBook() {
+//    }
+//
+//    @Test
+//    fun searchLongNamed() {
+//    }
+//
+//    @Test
+//    fun searchShortNamed() {
+//    }
 }
