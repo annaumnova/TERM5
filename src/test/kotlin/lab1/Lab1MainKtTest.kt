09b9b579a10eed25a3e04fbf74ed2f82c573f602
Anna <0301annaumnova@gmail.com>
2022-03-21 23:40:18 +0300

    it's finished

--- src/test/kotlin/lab1/Lab1MainKtTest.kt
+++ src/test/kotlin/lab1/Lab1MainKtTest.kt
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Assertions.*
 internal class Lab1MainKtTest {
     //    private val bookOne = BooksLib("Мы","Замятин Е.И.","1924")
     //    private val bookTwo = BooksLib("Ионыч","Чехов А.П.","1898")
+
     @Test
     fun parseBookList() {
         val firstBook = "1.Мы//Замятин Е.И.//1924"
@@ -29,17 +30,41 @@ internal class Lab1MainKtTest {
 
     @Test
     fun searchNewBook() {
+        val NewBook = "1.Мы//Замятин Е.И.//1924\n" +
+                "2.Ионыч//Чехов А.П.//1898"
+        assertEquals(
+            BooksLib("Мы", "Замятин Е.И.", "1924"),
+            searchNewBook(parseBookList(NewBook))
+        )
     }
-//
-//    @Test
-//    fun searchOldBook() {
-//    }
-//
-//    @Test
-//    fun searchLongNamed() {
-//    }
-//
-//    @Test
-//    fun searchShortNamed() {
-//    }
+
+    @Test
+    fun searchOldBook() {
+        val OldBook = "1.Мы//Замятин Е.И.//1924\n" +
+                "2.Ионыч//Чехов А.П.//1898"
+        assertEquals(
+            BooksLib("Ионыч", "Чехов А.П.", "1898"),
+            searchOldBook(parseBookList(OldBook))
+        )
+    }
+
+    @Test
+    fun searchLongNamed() {
+        val LongBook = "1.Мы//Замятин Е.И.//1924\n" +
+                "2.Ионыч//Чехов А.П.//1898"
+        assertEquals(
+            BooksLib("Ионыч", "Чехов А.П.", "1898"),
+            searchLongNamed(parseBookList(LongBook))
+        )
+    }
+
+    @Test
+    fun searchShortNamed() {
+        val ShortBook = "1.Мы//Замятин Е.И.//1924\n" +
+                "2.Ионыч//Чехов А.П.//1898"
+        assertEquals(
+            BooksLib("Мы", "Замятин Е.И.", "1924"),
+            searchShortNamed(parseBookList(ShortBook))
+        )
+    }
 }
