7275166c4fbf48a6ac62d199dffb38824530cd72
Anna <0301annaumnova@gmail.com>
2022-04-10 21:13:34 +0300

    just do it

---
+++ src/test/kotlin/lab2/ShapeCollectorTest.kt
@@ -0,0 +1,52 @@
+package lab2
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class ShapeCollectorTest {
+
+    @Test
+    fun addFigures() {
+    }
+
+    @Test
+    fun minSquareOfFigure() {
+    }
+
+    @Test
+    fun maxSquareOfFigure() {
+    }
+
+    @Test
+    fun summarizedSquare() {
+    }
+
+    @Test
+    fun searchColorBorder() {
+    }
+
+    @Test
+    fun searchFillColor() {
+    }
+
+    @Test
+    fun getListofFigures() {
+    }
+
+    @Test
+    fun getAmountofFigures() {
+    }
+
+    @Test
+    fun getGroupBorder() {
+    }
+
+    @Test
+    fun getGroupFill() {
+    }
+
+    @Test
+    fun getFigureofClass() {
+    }
+}
