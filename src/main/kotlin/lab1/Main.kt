98aff5797d7fa27fa64250e03d34134c4636bf8e
Anna <0301annaumnova@gmail.com>
2022-12-27 19:01:03 +0300

    .

--- src/main/kotlin/lab1/Main.kt
+++ src/main/kotlin/lab1/Main.kt
@@ -1,2 +1,13 @@
 package lab1
 
+fun main() {
+    var flag = true
+    while (flag) {
+        val question = SearchRequest()
+        question.searchingWiki()
+
+        println("New quest? Enter YES or NO.")
+        if (readln() == "NO")
+            flag = false
+    }
+}
